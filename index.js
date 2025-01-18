73032122fde5d6231654447e62902b38d8e2e6f1
tildejustin <spacepepper3.14@gmail.com>
2025-01-17 19:15:44 -0500

    april fools mods support

--- index.js
+++ index.js
@@ -37,21 +37,42 @@ function initVersions() {
 
 function initResources() {
     $('#resources-tab').addClass('collapsible');
-    $('#resources-tab').html(mods.filter(mod => mod.versions.find(version => version.target_version.find(gv => satisfiesVersion(gv)) && rulesCheck(mod))).map(mod => getElementFromModInfo(mod)).join(''));
+    $('#resources-tab').html(mods.filter(mod => mod.versions.find(version => version.target_version.find(gv => gv == typeOptions.version) && rulesCheck(mod))).map(mod => getElementFromModInfo(mod)).join(''));
     var fabric_text
-    if (compareVersions(typeOptions.version, "1.13.2") == 1) {
+    if (compareVersion(typeOptions.version, "1.13.2") == 1) {
         fabric_text = "The following mods use the <a href=\"https://fabricmc.net/use/installer/\">Fabric Loader</a>."
     } else {
         fabric_text = "The following mods use <a href=\"https://legacyfabric.net/downloads.html\">Legacy Fabric</a>. Legal MultiMC instance <a href=\"https://github.com/Minecraft-Java-Edition-Speedrunning/legacy-fabric-instance-generator/releases\">here</a>."
     }
     var optifine_text = ""
-    if (compareVersions(typeOptions.version, "1.7.2") <= 0) {
+    if (compareVersion(typeOptions.version, "1.7.2") <= 0) {
         optifine_text = "\n <a href=\"https://github.com/speedycube64/Complete_OptiFine_Archive_Pre_1.9\">Optifine</a> is allowed for this version and is a dependency of Optifabric."
-    } else if (compareVersions(typeOptions.version, "1.14.4") <= 0) {
+    } else if (compareVersion(typeOptions.version, "1.14.4") <= 0) {
         optifine_text = "\n <a href=\"https://optifine.net/home\">Optifine</a> is allowed for this version and is a dependency of Optifabric."
     }
     $("#fabric_text").html(fabric_text)
     $("#optifine_text").html(optifine_text)
+}
+
+// heh, shouldn't hardcode this stuff but it's just for the fabric link so it's fine for now
+versionMap = new Map([
+    ["15w14a", "1.8.2"],
+    ["1.RV-Pre1", "1.9.2"],
+    ["20w14infinite", "1.15.2"],
+    ["22w13oneblockatatime", "1.18.2"],
+    ["23w13a_or_b", "1.19.4"],
+    ["24w14potato", "1.20.4"]
+])
+
+function compareVersion(one, two) {
+    if (versionMap.get(one) != undefined) one = versionMap.get(one)
+    if (versionMap.get(two) != undefined) two = versionMap.get(two)
+    try {
+        return compareVersions(one, two)
+    } catch (error) {
+        console.log("not good", one, two)
+        return 0 // safe case at least
+    }
 }
 
 $(document).ready(() => {
@@ -109,7 +130,7 @@ $('[id^="mod-version-"]').click(function () {
 
 // Return Mod HTML String
 function getElementFromModInfo(mod) {
-    const build = mod.versions.find(mod_version => mod_version.target_version.find(gv => satisfiesVersion(gv)) && rulesCheck(mod));
+    const build = mod.versions.find(version => version.target_version.find(gv => gv == typeOptions.version) && rulesCheck(mod))
     return `<li>` +
         //Mod Name & Version & Mod Loader
         `<div class="collapsible-header light-font"><b>${mod.name}</b>${`<small style="padding-left: 0.5em;">(${build.version})</small>`}</div>` +
@@ -127,18 +148,9 @@ function getElementFromModInfo(mod) {
         //Page Button
         (mod.sources ? `<div style="padding: 5px;"><a class="light-font waves-effect waves-light btn" href="${mod.sources}" target="_blank">Sources</a></div>` : '') +
         (mod.sources ? `<div style="padding: 5px;"><a class="light-font waves-effect waves-light btn" href="${mod.sources}/issues" target="_blank">Issues</a></div>` : '') +
-        `</div>` + 
+        `</div>` +
         `</div>` +
         `</li>`;
-}
-
-function satisfiesVersion(game_version) {
-    try {
-        return satisfies(typeOptions.version, game_version);
-    } catch (e) {
-        console.log(e)
-    }
-
 }
 
 function rulesCheck(mod) {
